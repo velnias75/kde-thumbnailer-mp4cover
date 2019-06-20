dcecafd5d6683f9c5e9f00ae18915438ef110b66
Heiko Schaefer <heiko@rangun.de>
2019-06-20 14:59:26 +0200

    Initial commit

---
+++ mp4coverthumbnailer.cpp
@@ -0,0 +1,50 @@
+/*
+This file is part of mp4coverthumbnailer
+Copyright (C) 2019 Heiko Schäfer <heiko@rangun.de>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "mp4coverthumbnailer.h"
+
+#include <QtGui/QImage>
+#include <taglib/mp4file.h>
+
+extern "C" {
+    Q_DECL_EXPORT ThumbCreator *new_creator() {
+        return new MP4CoverThumbnailer();
+    }
+}
+
+bool MP4CoverThumbnailer::create(const QString &path, int, int, QImage &img) {
+    
+    TagLib::MP4::File f(path.toStdString().c_str(), false);
+    TagLib::MP4::Tag* tag = f.tag();
+    TagLib::MP4::ItemListMap itemsListMap = tag->itemListMap();
+    TagLib::MP4::Item coverItem = itemsListMap["covr"];
+    TagLib::MP4::CoverArtList coverArtList = coverItem.toCoverArtList();
+    
+    if(!coverArtList.isEmpty()) {
+        TagLib::MP4::CoverArt coverArt = coverArtList.front();
+        img.loadFromData((const uchar *)coverArt.data().data(), 
+                         coverArt.data().size());    
+        return true;
+    }
+    
+    return false;
+}
+
+ThumbCreator::Flags MP4CoverThumbnailer::flags() const {
+    return ThumbCreator::BlendIcon;
+}
---
+++ mp4coverthumbnailer.h
@@ -0,0 +1,35 @@
+/*
+This file is part of mp4coverthumbnailer
+Copyright (C) 2019 Heiko Schäfer <heiko@rangun.de>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef MP4COVERTHUMBNAILER_H
+#define MP4COVERTHUMBNAILER_H
+
+#include <QObject>
+#include <kio/thumbcreator.h>
+
+class MP4CoverThumbnailer : public QObject, public ThumbCreator
+{
+    Q_OBJECT
+
+    public:
+        virtual bool create(const QString &path, int width, int height, QImage &img) override;
+        virtual Flags flags() const override;
+
+};
+
+#endif // MP4COVERTHUMBNAILER_H
